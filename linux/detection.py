98284c4b7aae2b825b904116c1313760ca3a4de6
Muhammad <awesomeagent80@gmail.com>
2025-12-18 14:42:07 +1100

    update 4

--- linux/detection.py
+++ linux/detection.py
@@ -140,7 +140,17 @@ class DetectionSystem:
                 
                 # Get label
                 class_id = int(box.cls[0])
-                labels = ['person','car','bicycle','motorcycle','bus','truck','dog','cat','bird']
+                labels = ['person','bicycle','car','motorcycle','airplane','bus','train','truck','boat',
+                            'traffic light','fire hydrant','stop sign','parking meter','bench','bird','cat',
+                            'dog','horse','sheep','cow','elephant','bear','zebra','giraffe','backpack',
+                            'umbrella','handbag','tie','suitcase','frisbee','skis','snowboard','sports ball',
+                            'kite','baseball bat','baseball glove','skateboard','surfboard','tennis racket',
+                            'bottle','wine glass','cup','fork','knife','spoon','bowl','banana','apple',
+                            'sandwich','orange','broccoli','carrot','hot dog','pizza','donut','cake','chair',
+                            'couch','potted plant','bed','dining table','toilet','tv','laptop','mouse',
+                            'remote','keyboard','cell phone','microwave','oven','toaster','sink','refrigerator',
+                            'book','clock','vase','scissors','teddy bear','hair drier','toothbrush'
+                        ]
                 label = labels[class_id] if class_id < len(labels) else 'object'
                 
                 detections.append({
