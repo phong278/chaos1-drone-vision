f38b08be3ad732fd2c2077f54a7209be379baaae
Muhammad <awesomeagent80@gmail.com>
2026-01-06 10:14:21 +1100

    fixed bug

--- linux/detection.py
+++ linux/detection.py
@@ -120,7 +120,17 @@ class DetectionSystem:
     def detect(self, frame):
         """Run YOLOv8 detection on frame"""
         inference_size = self.config['performance']['inference_size']
-        
+        labels = ['person','bicycle','car','motorcycle','airplane','bus','train','truck','boat',
+                            'traffic light','fire hydrant','stop sign','parking meter','bench','bird','cat',
+                            'dog','horse','sheep','cow','elephant','bear','zebra','giraffe','backpack',
+                            'umbrella','handbag','tie','suitcase','frisbee','skis','snowboard','sports ball',
+                            'kite','baseball bat','baseball glove','skateboard','surfboard','tennis racket',
+                            'bottle','wine glass','cup','fork','knife','spoon','bowl','banana','apple',
+                            'sandwich','orange','broccoli','carrot','hot dog','pizza','donut','cake','chair',
+                            'couch','potted plant','bed','dining table','toilet','tv','laptop','mouse',
+                            'remote','keyboard','cell phone','microwave','oven','toaster','sink','refrigerator',
+                            'book','clock','vase','scissors','teddy bear','hair drier','toothbrush'
+                        ]
         
         target_classes = ['person', 'bird', 'bicycle', 'car', 'truck']  # adapt for drones, trees
         class_ids = [labels.index(c) for c in target_classes if c in labels]
@@ -139,17 +149,7 @@ class DetectionSystem:
                 
                 # Get label
                 class_id = int(box.cls[0])
-                labels = ['person','bicycle','car','motorcycle','airplane','bus','train','truck','boat',
-                            'traffic light','fire hydrant','stop sign','parking meter','bench','bird','cat',
-                            'dog','horse','sheep','cow','elephant','bear','zebra','giraffe','backpack',
-                            'umbrella','handbag','tie','suitcase','frisbee','skis','snowboard','sports ball',
-                            'kite','baseball bat','baseball glove','skateboard','surfboard','tennis racket',
-                            'bottle','wine glass','cup','fork','knife','spoon','bowl','banana','apple',
-                            'sandwich','orange','broccoli','carrot','hot dog','pizza','donut','cake','chair',
-                            'couch','potted plant','bed','dining table','toilet','tv','laptop','mouse',
-                            'remote','keyboard','cell phone','microwave','oven','toaster','sink','refrigerator',
-                            'book','clock','vase','scissors','teddy bear','hair drier','toothbrush'
-                        ]
+                
                 label = labels[class_id] if class_id < len(labels) else 'object'
                 detections.append({
                     'label': label,
